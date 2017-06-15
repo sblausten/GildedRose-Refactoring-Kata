4b07846c33c123b1b8363702f16f9a353fe916ad
Sam <sblausten@Sams-Mac.local>
2017-06-15 12:16:23 +0100

    Added some unit tests for item class and fixed example test for update_quality

---
+++ ruby/spec/item_spec.rb
@@ -0,0 +1,18 @@
+require './gilded_rose'
+
+describe Item do
+  describe '#new' do
+  	before(:each) do
+  		@item = Item.new("name", 5, 7)
+  	end
+  	it 'saves name as instance variable on initialization' do
+  		expect(@item.name).to eq "name"
+  	end
+  	it 'saves sell_in as instance variable on initialization' do
+  		expect(@item.sell_in).to eq 5
+  	end
+  	it 'saves quality as instance variable on initialization' do
+  		expect(@item.quality).to eq 7
+  	end
+  end
+end
