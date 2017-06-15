70ccf943ffabdb96f011af281cf9210d2c7d45bc
Sam <sblausten@Sams-Mac.local>
2017-06-15 12:24:17 +0100

    Added more unit tests covering basic functionality for standard items

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -3,10 +3,16 @@ require './gilded_rose'
 describe GildedRose do
 
   describe "#update_quality" do
+  	before(:each) do
+  		@items = [Item.new("foo", 0, 0), Item.new("foo", 6, 5)]
+  	end
     it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "foo"
+      GildedRose.new(@items).update_quality()
+      expect(@items[0].name).to eq "foo"
+    end
+    it "reduces the sell_in counter by 1" do
+      GildedRose.new(@items).update_quality()
+      expect(@items[1].sell_in).to eq 5
     end
   end
 
