952b6b33c86ab81736b927499ea4efc5d18c79da
Sam <sblausten@Sams-Mac.local>
2017-06-15 15:32:49 +0100

    Further refactor

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -9,17 +9,24 @@ class GildedRose
 
   def update_items()
     @items.each do |item|
-      quality(item)
-      sell_in(item)
+      update_quality(item)
+      update_sell_in(item)
     end
   end
 
   private
 
-  def quality(item)
-    if item.name == "Aged Brie"
-      item.quality += 1
-    elsif item.name.start_with?("Backstage passes")
+  def update_quality(item)
+    item.quality -= 1 unless check_backstage_pass(item) ||
+      check_aged_brie(item) ||
+      check_conjured(item) ||
+      check_sulfuras(item) ||
+      check_out_of_date(item)
+    enforce_quality_limits(item)
+  end
+
+  def check_backstage_pass(item)
+    if item.name.start_with?("Backstage passes")
       if item.sell_in <= 0
         item.quality = 0
       elsif item.sell_in <= 5
@@ -29,19 +36,31 @@ class GildedRose
       else
         item.quality += 1
       end
-    elsif item.name.start_with?("Conjured")
-      item.quality -= 2
-    elsif item.name == "Sulfuras, Hand of Ragnaros"
-    elsif item.sell_in <= 0
-      item.quality -= 2
-    else
-      item.quality -= 1
     end
+  end
+
+  def check_sulfuras(item)
+    item.name == "Sulfuras, Hand of Ragnaros"
+  end
+
+  def check_aged_brie(item)
+    item.quality += 1 if item.name == "Aged Brie"
+  end
+
+  def check_conjured(item)
+    item.quality -= 2 if item.name.start_with?("Conjured") 
+  end
+
+  def check_out_of_date(item)
+    item.quality -= 2 if item.sell_in <= 0
+  end
+
+  def enforce_quality_limits(item)
     item.quality = MAX_QUALITY if item.quality > MAX_QUALITY
     item.quality = MIN_QUALITY if item.quality < MIN_QUALITY
   end
 
-  def sell_in(item)
+  def update_sell_in(item)
     item.sell_in -= 1 unless item.name == "Sulfuras, Hand of Ragnaros"
   end
 end
